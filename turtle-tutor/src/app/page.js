3f062add816ae229f778df026aa6edffefe20c66
Gabriel Nixon <gabecn30@icloud.com>
2024-09-15 10:29:45 -0400

    PAGE 1 COMPLETELY FINISHED.

--- turtle-tutor/src/app/page.js
+++ turtle-tutor/src/app/page.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import React, { useState, useEffect} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useRouter } from 'next/navigation';  // Updated to use the App Router's useRouter
+import logo from './static/logo.svg';
 
 
 
@@ -33,45 +34,62 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 style={{ color: "#468585", fontSize: "4em", justifyContent: "center", fontWeight: "bold", letterSpacing: ".3em" }} className="items-center">
-          turtle tutor
-        </h1>
-        <form id = "form" onSubmit = {handleSubmit}>
-          <div className="items-center justify-items-center">
-            <p style={{ color: "#000", fontSize: "1.5em"}}>Select a
-            <select onChange = {(e) => setHardTopic(e.target.value)} name = "topic" style={{ paddingLeft: "0.5em", marginLeft: "0.5em", borderWidth: "0.10em", borderRadius: "0.4em", borderColor: "#808080"}} className="topic">
-              <option value="java">Topic</option>
-              <option value="java">Java</option>
-              <option value="python">Python</option>
-              <option value="javascript">Javascript</option>
-            </select>
-            </p>
+        {/* Main content */}
+        <div id="main-content">
+          {/* Left column (homepage content) */}
+          <div id ="left-column">
+            {/* Main title */}
+            <h1 id="main-title-content" style={{ color: "#468585", fontSize: "5.5em", justifyContent: "center", fontWeight: "bold", letterSpacing: ".3em" }} className="items-center">
+              turtle tutor
+            </h1>
+
+            {/* Topic Selection */}
+            <form id = "form" onSubmit = {handleSubmit}>
+              <div  id="main-title-content" className="w-1/10 mx-auto items-center justify-items-center" style={{ color: "#000", fontSize: "1.5em"}}>
+                {/* Dropdown */}
+                <p>Select a
+                <select onChange = {(e) => setHardTopic(e.target.value)} name = "topic" style={{ paddingLeft: "0.5em", marginLeft: "0.5em", marginRight: "0.5em", borderWidth: "0.10em", borderRadius: "0.4em", borderColor: "#808080"}} className="topic">
+                  <option value="java">Topic</option>
+                  <option value="java">Java</option>
+                  <option value="python">Python</option>
+                  <option value="javascript">Javascript</option>
+                </select>
+                  or 
+                  <input id = "manual" name = "manual" disabled = {hardTopic != ""} className = {hardTopic != "" ?  "ml-2 pl-2 " : ""}
+                  style= {{ paddingLeft: "0.5em", marginLeft: "0.5em", borderWidth: "0.10em", borderRadius: "0.4em", borderColor: "#808080"}} placeholder="type your topic here." />
+                </p>
+              </div>
+
+              {/* Submission button */}
+              <div id="submission-button-content" className="w-1/2 mx-auto">
+                <button 
+                  id="submission-button"
+                  type = "submit"
+                  className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+                  style={{ backgroundColor: "#468585" }}
+                >
+                  Start Review!
+                </button>
+              </div>
+            </form>
           </div>
-        <div>
-          <p style={{ color: "#000", fontSize: "1.5em"}}>
-            or 
-            <input id = "manual" name = "manual" disabled = {hardTopic != ""} className = {hardTopic != "" ?  "ml-2 pl-2 " : ""}
-            style= {{ paddingLeft: "0.5em", marginLeft: "0.5em", borderWidth: "0.10em", borderRadius: "0.4em", borderColor: "#808080"}} />
-          </p>
-        </div>
 
-        <div>
-          <p style={{ color: "#000", fontSize: "1.5em"}}>
-            Class Size: 
-            <input style={{ paddingLeft: "0.5em", marginLeft: "0.5em", borderWidth: "0.10em", borderRadius: "0.4em", borderColor: "#808080"}} />
-          </p>
+          {/* Right column (main logo) */}
+          <Image
+            id="right-column"
+            src={logo}
+            alt="The tutor turtle logo."
+            width="600px"
+            height="auto" />
         </div>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <button type = "submit"
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            style={{ backgroundColor: "#468585" }}
-          >
-            Go!
-          </button>
-        </div>
-        </form>
       </main>
+
+        {/* Credits footer */}
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <p style={{color: "#50B498", fontSize: "1.0em"}}>
+          Created by Amisha, Jordan, Tory, and Gabe @HackMIT 2024! =)
+        </p>
+      </footer>
     </div>
   );
 }
