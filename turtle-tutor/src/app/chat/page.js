22f42310ef1b9865f9f8d3b4b68b4b512778d311
Gabriel Nixon <gabecn30@icloud.com>
2024-09-15 09:11:05 -0400

    Finalized PAGE 2 content.

--- turtle-tutor/src/app/chat/page.js
+++ turtle-tutor/src/app/chat/page.js
@@ -25,32 +25,102 @@ const Chat = () => {
   };
 
   return (
-    <div className="chat-container">
-      <div className="chat-box">
-        {messages.map((message, index) => (
-          <div key={index} className={`bubble ${message.sender}`}>
-            <p>{message.text}</p>
+    // ALL CONTENT
+    <div id="main-content-container">
+      {/* Left column */}
+      <div id="left-column">
+        {/* Live feedback text */}
+        <div id="live-feedback-content">
+          Live Feedback
+        </div>
+
+        {/* Turtle container */}
+        <div id="turtle-container">
+          {/* Turtle #1 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
           </div>
-        ))}
+          
+          {/* Turtle #2 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
+          </div>
+
+          {/* Turtle #3 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
+          </div>
+
+          {/* Turtle #4 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
+          </div>
+
+          {/* Turtle #5 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
+          </div>
+
+          {/* Turtle #6 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
+          </div>
+
+          {/* Turtle #7 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
+          </div>
+
+          {/* Turtle #8 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
+          </div>
+
+          {/* Turtle #9 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
+          </div>
+
+          {/* Turtle #10 */}
+          <div>
+            <img className="turtle-image" src="static/turtle-symbol-grey.svg"></img>
+          </div>
+        </div>
       </div>
 
-      <div className="input-container">
-        <input
-          type="text"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-          placeholder="Type a message..."
-        />
-        <button onClick={handleSendMessage} className="send-button">
-          ➔
-        </button>
-      </div>
+      {/* Right column */}
+      <div id="right-column">
+        {/* Chat container */}
+        <div className="chat-container">
+          {/* Chat box */}
+          <div className="chat-box">
+            {messages.map((message, index) => (
+              <div key={index} className={`bubble ${message.sender}`}>
+                <p>{message.text}</p>
+              </div>
+            ))}
+          </div>
 
-      {/* End Session Button */}
-      <div className="end-session-container">
-        <button onClick={handleEndSession} className="end-session-button">
-          End Session
-        </button>
+          {/* Input container */}
+          <div className="input-container">
+            <input
+              type="text"
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+              placeholder="Type a message..."
+            />
+            <button onClick={handleSendMessage} className="send-button">
+              ➔
+            </button>
+          </div>
+
+          {/* End Session Button */}
+          <div className="end-session-container">
+            <button onClick={handleEndSession} className="end-session-button">
+              End Session
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
